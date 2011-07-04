fcc9d26e9621971160c89c398a49316909816755
Pradeep Ranganathan <rpradeep@umich.edu>
2011-07-04 16:03:21 -0400

    Just display frame instead of model image

--- java/src/distort/gen/GenerativeProjection.java
+++ java/src/distort/gen/GenerativeProjection.java
@@ -16,29 +16,26 @@ import april.lcmtypes.*;
 import april.util.*;
 import april.vis.*;
 
+import magic.vis.chart.*;
+
 
 public class GenerativeProjection implements LCMSubscriber
 {
-    JFrame jf;
-    JImage jim;
-    Model model;
+    final VisWorld.Buffer vb;
 
     public GenerativeProjection(String modelFile) throws IOException
     {
-        BufferedImage mim = ImageIO.read(new File(modelFile));
-        model = new Model(ImageUtil.conformImageToInt(mim));
-
-        jf = new JFrame();
+        JFrame jf = new JFrame();
         jf.setLayout(new BorderLayout());
 
-        jim = new JImage();
-        jf.add(jim, BorderLayout.NORTH);
-
         VisCanvas vc = new VisCanvas(new VisWorld());
-        jf.add(vc, BorderLayout.SOUTH);
+        vc.setBackground(Color.black);
+        jf.add(vc, BorderLayout.CENTER);
 
+        vb = vc.getWorld().getBuffer("projection");
+
+        jf.setSize(800, 800);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setSize(640, 480);
         jf.setVisible(true);
 
         LCM.getSingleton().subscribe("HOMOGRAPHY", this);
@@ -56,122 +53,33 @@ public class GenerativeProjection implements LCMSubscriber
                 for (int j=0; j<3; ++j)
                     h[i][j] = dins.readDouble();
 
-            System.out.println("Generating image ...");
-            BufferedImage im = project(new Homography(h));
-            jim.setImage(im);
-            jim.repaint();
-            jf.invalidate();
+            Homography hmg = new Homography(h);
+
+            ArrayList<double[]> points = new ArrayList<double[]>();
+            points.add(hmg.project(-.1, -.1));
+            points.add(hmg.project(+.1, -.1));
+            points.add(hmg.project(+.1, +.1));
+            points.add(hmg.project(-.1, +.1));
+            points.add(hmg.project(-.1, -.1));
+            points.add(hmg.project(+.1, +.1));
+            points.add(hmg.project(-.1, +.1));
+            points.add(hmg.project(+.1, -.1));
+
+            VisPlot plot = new VisPlot();
+            plot.setXAxis(ChartAxis.makeXAxis(-600, 600, 50));
+            plot.setYAxis(ChartAxis.makeYAxis(-600, 600, 50));
+            plot.addData(new XYDataSeries(points, new VisDataLineStyle(Color.cyan, 2)));
+            vb.addBuffered(plot);
+            vb.switchBuffer();
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    BufferedImage project(Homography h)
-    {
-        /* Project points from the model into image space */
-        ArrayList<double[]> proj = new ArrayList<double[]>();
-        for (double[] xyc : model.getSamples()) {
-            double[] pxy = LinAlg.resize(h.project(xyc[0], xyc[1]), 3);
-            pxy[2] = xyc[2];
-            proj.add(pxy);
-        }
-
-        /* Grid the projected points for efficient search */
-        final double x0 = -600, y0 = -600, x1 = 600, y1 = 600;
-        Gridder<double[]> grid = new Gridder<double[]>(x0, y0, x1, y1, 2);
-
-        for (double[] v : proj) {
-            double[] xyrgb = new double[5];
-            xyrgb[0] = v[0];
-            xyrgb[1] = v[1];
-
-            int c = (int) v[2];
-            xyrgb[2] = ((c & 0xFF0000) >>> 16) / 255.0;
-            xyrgb[3] = ((c & 0xFF00) >>> 8) /255.0;
-            xyrgb[4] = (c & 0xFF) / 255.0;
-
-            grid.add(v[0], v[1], xyrgb);
-        }
-
-        /* Interpolate the resulting image */
-        BufferedImage im = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
-        final int W = im.getWidth();
-        final int H = im.getHeight();
-
-        for (int y = -H/2; y < H/2; ++y)
-            for (int x = -W/2; x < W/2; ++x) {
-                Iterable<double[]> nbors = grid.find(x, y, 2);
-                im.setRGB(x+W/2, y+H/2, interpolate(new double[] {x,y}, nbors, 2));
-            }
-
-        return im;
-    }
-
-    static int interpolate(double[] xy, Iterable<double[]> neighbors, double range)
-    {
-        double sumWt = 0;
-        double[] rgb = new double[3];
-
-        for (double[] xyrgb : neighbors) {
-             double z = LinAlg.squaredDistance(xy, xyrgb) / (range*range);
-             double wt = Math.exp(-z);
-             LinAlg.plusEquals(rgb, LinAlg.scale(LinAlg.copy(xyrgb, 2, 3), wt));
-
-             sumWt += wt;
-        }
-
-        if (sumWt < 1e-8) {
-            return Color.TRANSLUCENT;
-        }
-
-        rgb = LinAlg.scale(rgb, 1/sumWt);
-        return new Color((float)rgb[0], (float)rgb[1], (float)rgb[2]).getRGB();
-    }
-
     public static void main(String[] args) throws IOException
     {
         new GenerativeProjection(args[0]);
-    }
-}
-
-class Model
-{
-    final BufferedImage im;
-
-    final double width = 0.2;
-    final double height = 0.2;
-
-    public Model(BufferedImage im)
-    {
-        this.im = im;
-    }
-
-    public List<double[]> getSamples()
-    {
-        final int W = im.getWidth();
-        final int H = im.getHeight();
-        final int pixels[] = ((DataBufferInt) (im.getRaster().getDataBuffer())).getData();
-
-        ArrayList<double[]> samples = new ArrayList<double[]>();
-
-        for (int y = 0; y < H; ++y)
-            for (int x = 0; x < W; ++x) {
-                double[] xy = xform(x, y);
-                double[] pxyc = LinAlg.resize(xy, 3);
-                pxyc[2] = pixels[y*W+x] | (0xFF << 24);
-                samples.add(pxyc);
-            }
-
-        return samples;
-    }
-
-    double[] xform(double x, double y)
-    {
-        final double W = im.getWidth();
-        final double H = im.getHeight();
-
-        return new double[] {(x-W/2)/W * width, (y-H/2)/H * height};
     }
 }
 
